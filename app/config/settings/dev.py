a50515271baaa97948bef983fde83de94e0ae8ed
hanyonghee <hanyonghee9264@gmail.com>
2019-09-17 23:08:41 +0900

    [UPDATE] postgresql db 정보 secrets 이동

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -4,12 +4,5 @@ DEBUG = True
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'db-first-person.crsxgsctsjxm.ap-northeast-2.rds.amazonaws.com',
-        'USER': 'fastkim0909',
-        'PASSWORD': 'rlavozoa12',
-        'PORT': 5432,
-    }
-}
+secrets = json.load(open(SECRETS_DIR, 'dev.json'))
+
