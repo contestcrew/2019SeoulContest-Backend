6254d2820da7c9f72ca18e456491d22a2e6aca63
DiceMono <qrdfg@naver.com>
2019-09-20 22:02:35 +0900

    add request Model

---
+++ app/request/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField('이름', max_length=30)
+    score = models.PositiveIntegerField('점수', default=0)
+    image = models.ImageField(upload_to='category')
+
+
+class Request(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='카테고리')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='작성자')
+    title = models.CharField('제목', maxlength=100)
+    content = models.TextField('내용')
+    score = models.PositiveIntegerField('점수', default=0)
+    main_address = models.CharField()
+    detail_address = models.CharField()
+    latitude = models.FloatField('위도', blank=True)
+    longitude = models.FloatField('경도', blank=True)
+    occurred_at = models.DateField('발생 시각')
+    created_at = models.DateTimeField('업로드 시각', auto_now_add=True)
+    updated_at = models.DateTimeField('수정 시각', auto_now=True)
+
+
+class RequestImage(models.Model):
+    request = models.ForeignKey(Request, related_name='images', blank=True, null=True,
+                                on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='request/%Y/%m/%d', verbose_name='이미지')
