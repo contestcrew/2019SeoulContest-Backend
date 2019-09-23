cdfef739a418499cf8843c49a3e4fbdec6474a33
DiceMono <qrdfg@naver.com>
2019-09-23 19:49:39 +0900

    add status field

--- app/request/models.py
+++ app/request/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
 User = get_user_model()
 
@@ -11,10 +13,16 @@ class Category(models.Model):
 
 
 class Request(models.Model):
+    REQUEST_STATUS = Choices(
+        (0, "REQUEST", _("REQUEST")),
+        (1, "PROGRESS", _("PROGRESS")),
+        (2, "COMPLETE", _("COMPLETE")),
+    )
     category = models.ForeignKey(Category, related_name='requests', on_delete=models.CASCADE, verbose_name='카테고리')
     author = models.ForeignKey(User, related_name='requests', on_delete=models.CASCADE, verbose_name='작성자')
     title = models.CharField('제목', max_length=100)
     content = models.TextField('내용')
+    status = models.IntegerField('상태', choices=REQUEST_STATUS, default=REQUEST_STATUS.REQUEST)
     score = models.PositiveIntegerField('점수', default=0)
     main_address = models.CharField('메인 주소', max_length=30)
     detail_address = models.CharField('상세 주소', max_length=50)
