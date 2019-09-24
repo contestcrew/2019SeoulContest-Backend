0479f74ac1199eb6fadc0accabf53dca5541433e
DiceMono <qrdfg@naver.com>
2019-09-24 21:47:41 +0900

    add Model in Admin Page

--- app/request/admin.py
+++ app/request/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Request, RequestImage
+from .models import Category, Request, RequestImage, PoliceOffice
 
+admin.site.register(PoliceOffice)
 admin.site.register(Category)
 admin.site.register(Request)
 admin.site.register(RequestImage)
