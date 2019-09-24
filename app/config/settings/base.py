e1a6b41c6147ae5e6cb1a377d568da25b9a813a1
hanyonghee <hanyonghee9264@gmail.com>
2019-09-25 01:22:23 +0900

    [DELETE] installed_apps django swagger 삭제

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework_swagger",
     "rest_framework.authtoken",
     "rest_framework",
     "django_extensions",
