243f044338e45540b687f477dddfd1ce98f73038
hanyonghee <hanyonghee9264@gmail.com>
2019-09-25 00:52:17 +0900

    [FIX] installed_apps swagger 추가

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework_swagger",
     "rest_framework.authtoken",
     "rest_framework",
     "django_extensions",
