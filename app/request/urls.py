a5921b03f261d831cad4fd5dfe0883095b905723
DiceMono <qrdfg@naver.com>
2019-09-25 01:49:06 +0900

    fix police url

--- app/request/urls.py
+++ app/request/urls.py
@@ -2,9 +2,9 @@ from rest_framework import routers
 from .views import CategoryViewSet, RequestViewSet, PoliceOfficeViewSet
 
 router = routers.SimpleRouter()
+router.register(r'police', PoliceOfficeViewSet)
 router.register(r'category', CategoryViewSet)
 router.register(r'', RequestViewSet)
-router.register(r'police', PoliceOfficeViewSet)
 
 urlpatterns = [
 ]
