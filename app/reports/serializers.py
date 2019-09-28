4e6cbee0d1695d3ee608ad0be8d10ce168bc3ee1
mongkyo <dreamong91@gmail.com>
2019-09-28 13:15:19 +0900

    Hotfix: notification에 import error수정

--- app/reports/serializers.py
+++ app/reports/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Report
-from app.notifications.utils import push_notifications
+from notifications.utils import push_notifications
 
 
 class ReportSerializer(serializers.ModelSerializer):
